eb7ae29a19396c66d2152473a17726323897c82e
andrey.rojas <andrey.rojas@sapiensdev.com>
2017-12-08 15:05:06 -0600

    commit - Add Task Types CRUD

---
+++ Persistence/Repositories/ITaskTypesRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Persistence.Repositories
+{
+    public interface ITaskTypesRepository:IDisposable
+    {
+        IEnumerable<TaskType> GetTaskTypes();
+        TaskType GetTaskTypeByID(int taskId);
+        void InsertTaskType(TaskType taskType);
+        void DeleteTaskType(int taskId);
+        void UpdateTaskType(TaskType taskType);
+        void Save();
+    }
+}
