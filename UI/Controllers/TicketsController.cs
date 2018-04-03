02aa52892e3460fc4fa517feab9eeb8b2ab82637
andrey.rojas <andrey.rojas@sapiensdev.com>
2018-04-03 02:50:14 -0600

    Surveys Part I

--- UI/Controllers/TicketsController.cs
+++ UI/Controllers/TicketsController.cs
@@ -11,6 +11,9 @@ using System;
 using PagedList;
 using log4net;
 using UI.toastr;
+using Microsoft.AspNet.Identity;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace UI.Controllers
 {
@@ -24,6 +27,7 @@ namespace UI.Controllers
         private static string statusToSend;
         private static double hoursResult;
         private static int idCustomerIndex;
+        private ApplicationUserManager _userManager;
 
         public TicketsController()
         {
@@ -378,6 +382,7 @@ namespace UI.Controllers
                     ticketRepo.UpdateTicket(ticket);
                     ticketRepo.Save();
                     this.AddToastMessage("Incidents", "Incident number " + ticket.Id + " updated to " + ticket.Status, ToastType.Success);
+                    sendSurveyEmail();
                 }
                 catch (Exception ex)
                 {
@@ -445,17 +450,172 @@ namespace UI.Controllers
 
         private void SendEmail()
         {
+            var user = UserManager.FindById(User.Identity.GetUserId());
             var apiKey = "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";
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("notifications@plusb.com", "PlusB Service Desk Notification");
-            var subject = "Status update Incident #  " + IDTicket;
+            var subject = "Status update in incident No.  " + IDTicket;
             var to = new EmailAddress(emailToSend, "Customer");
             var plainTextContent = "Incident Status update";
-            var htmlContent = "<h1 class='text-center'>The incident status was changed to " + statusToSend + "</h1 >";
+            var htmlContent = "<!doctype html> " +
+                                 "<html>" +
+                                   "<body style='background-color:#85A885;font-family:sans-serif;font-size:14px;-webkit-font-smoothing:antialiased;line-height:1.4;margin:0;padding:0;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;'>" +
+                                     "<br/><br/><table border='0' cellpadding='0'cellspacing='0' style='border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                      " <tr>" +
+                                         "<td>&nbsp;</td>" +
+                                         "<td style='display:block;margin:0 auto !important;max-width:580px;padding:10px;width:580px;'>" +
+                                           "<div style='box-sizing:border-box;display: block;margin:0 auto;max-width:580px;padding:10px;'>" +
+                                             "<table style='background:#ffffff;border-radius:3px;width:100%;border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                               "<tr>" +
+                                                 "<td style='box-sizing:border-box;padding:20px;'>" +
+                                                   "<table border='0' cellpadding='0'cellspacing='0' style='border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                                     "<tr>" +
+                                                       "<td style='font-family:sans-serif;font-size:14px;vertical-align:top;'>" +
+                                                         "<p>Hi there,</p>" +
+                                                         "<p>Status update occurred in an incident where you are owner. The user " + user.Email + " just updated to: </p>" +
+                                                         "<table border='0' cellpadding='0'cellspacing='0'>" +
+                                                           "<tbody>" +
+                                                             "<tr>" +
+                                                               "<td align='left'>" +
+                                                                 "<table border='0' cellpadding='0'cellspacing='0'>" +
+                                                                "   <tbody>" +
+                                                               "      <tr>" +
+                                                              "         <td style='text-align:center;'><a><strong>" + statusToSend + "</strong></a></td>" +
+                                                             "        </tr>" +
+                                                            "       </tbody>" +
+                                                           "      </table>" +
+                                                          "     </td>" +
+                                                         "    </tr>" +
+                                                        "   </tbody>" +
+                                                       "  </table>" +
+                                                      "   <br/>" +
+                                                      "   <p style='font-size:12px;'>This is an automatic email sent by the PlusB Consulting Service Desk system.</p>" +
+                                                     "    <p style='font-size:12px;'>Thanks for being part of our business!</p>" +
+                                                    "   </td>" +
+                                                   "  </tr>" +
+                                                 "  </table>" +
+                                               "  </td>" +
+                                              " </tr>" +
+                                             "</table>" +
+                                             "<br/>" +
+                                             "<div style='clear:both;margin-top:10px;width:100%;'>" +
+                                               "<table border='0' cellpadding='0' cellspacing='0' style='border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                                 "<tr><br/>" +
+                                                   "<td style='padding-bottom:10px;padding-top:10px;color:#000;font-size:12px;'>" +
+                                                     "<span style='color:#000;font-size:12px;'>PlusB Consulting Escazú, San José Costa Rica</span>" +
+                                                    " <br/> Don't like these emails? <a style='color:#000;font-size:12px;'>Unsubscribe</a>." +
+                                                  " </td>" +
+                                                 "</tr>" +
+                                                 "<tr>" +
+                                                   "<td style='padding-bottom:10px;padding-top:10px;text-decoration:none;color:#000;font-size:12px;'>" +
+                                                     "Powered by <a style='color:#000;font-size:12px;'>PlusB Consulting S.A</a>." +
+                                                   "</td>" +
+                                                 "</tr>" +
+                                               "</table>" +
+                                             "</div" +
+                                           "</div>" +
+                                         "</td>" +
+                                         "<td style='color:#000;font-size:12px;'>&nbsp;</td>" +
+                                       "</tr>" +
+                                     "</table>" +
+                                  "</body>" +
+                                 "</html>";
 
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = client.SendEmailAsync(msg);
         }
+
+        private void sendSurveyEmail()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("notifications@plusb.com", "PlusB Service Desk Survey");
+            var subject = "Satisfaction survey about incident No.  " + IDTicket;
+            var to = new EmailAddress(emailToSend, "Customer");
+            var plainTextContent = "Satisfaction survey";
+            var htmlContent = "<!doctype html> " +
+                                 "<html>" +
+                                   "<body style='background-color:#85A885;font-family:sans-serif;font-size:14px;-webkit-font-smoothing:antialiased;line-height:1.4;margin:0;padding:0;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;'>" +
+                                     "<br/><br/><table border='0' cellpadding='0'cellspacing='0' style='border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                      " <tr>" +
+                                         "<td>&nbsp;</td>" +
+                                         "<td style='display:block;margin:0 auto !important;max-width:580px;padding:10px;width:580px;'>" +
+                                           "<div style='box-sizing:border-box;display: block;margin:0 auto;max-width:580px;padding:10px;'>" +
+                                             "<table style='background:#ffffff;border-radius:3px;width:100%;border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                               "<tr>" +
+                                                 "<td style='box-sizing:border-box;padding:20px;'>" +
+                                                   "<table border='0' cellpadding='0'cellspacing='0' style='border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                                     "<tr>" +
+                                                       "<td style='font-family:sans-serif;font-size:14px;vertical-align:top;'>" +
+                                                         "<p>Hi there,</p>" +
+                                                         "<p>You were invited to evaluate the service provided by the consultant in charge of the incidente.</p>" +
+                                                         "<p>Please click the follwoing link</p>"+
+                                                         "<table border='0' cellpadding='0'cellspacing='0'>" +
+                                                           "<tbody>" +
+                                                             "<tr>" +
+                                                               "<td align='left'>" +
+                                                                 "<table border='0' cellpadding='0'cellspacing='0'>" +
+                                                                "   <tbody>" +
+                                                               "      <tr>" +
+                                                              "         <td style='text-align:center;'><a>http://localhost:65158/Surveys/Create</a></td>" +
+                                                             "        </tr>" +
+                                                            "       </tbody>" +
+                                                           "      </table>" +
+                                                          "     </td>" +
+                                                         "    </tr>" +
+                                                        "   </tbody>" +
+                                                       "  </table>" +
+                                                      "   <br/>" +
+                                                      "   <p style='font-size:12px;'>This is an automatic email sent by the PlusB Consulting Service Desk system.</p>" +
+                                                     "    <p style='font-size:12px;'>Thanks for being part of our business!</p>" +
+                                                    "   </td>" +
+                                                   "  </tr>" +
+                                                 "  </table>" +
+                                               "  </td>" +
+                                              " </tr>" +
+                                             "</table>" +
+                                             "<br/>" +
+                                             "<div style='clear:both;margin-top:10px;width:100%;'>" +
+                                               "<table border='0' cellpadding='0' cellspacing='0' style='border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;'>" +
+                                                 "<tr><br/>" +
+                                                   "<td style='padding-bottom:10px;padding-top:10px;color:#000;font-size:12px;'>" +
+                                                     "<span style='color:#000;font-size:12px;'>PlusB Consulting Escazú, San José Costa Rica</span>" +
+                                                    " <br/> Don't like these emails? <a style='color:#000;font-size:12px;'>Unsubscribe</a>." +
+                                                  " </td>" +
+                                                 "</tr>" +
+                                                 "<tr>" +
+                                                   "<td style='padding-bottom:10px;padding-top:10px;text-decoration:none;color:#000;font-size:12px;'>" +
+                                                     "Powered by <a style='color:#000;font-size:12px;'>PlusB Consulting S.A</a>." +
+                                                   "</td>" +
+                                                 "</tr>" +
+                                               "</table>" +
+                                             "</div" +
+                                           "</div>" +
+                                         "</td>" +
+                                         "<td style='color:#000;font-size:12px;'>&nbsp;</td>" +
+                                       "</tr>" +
+                                     "</table>" +
+                                  "</body>" +
+                                 "</html>";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+        #region public ApplicationUserManager UserManager
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        #endregion
 
         protected override void Dispose(bool disposing)
         {
